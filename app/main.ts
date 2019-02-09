c6aa1d17ddd40e79009cac70539d352cef396678
JayJayDee <jindongp@gmail.com>
2019-02-09 15:12:28 +0900

    added tslint configuration, added vuex stores.

--- app/main.ts
+++ app/main.ts
@@ -5,12 +5,11 @@ import Vuex from 'vuex';
 
 import './app.css';
 
-if(TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools);
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
-
 
 new Vue({
   render: h => h('frame', [h(App)])
