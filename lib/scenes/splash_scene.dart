156554260dac7f5650332277449c04bdcf30b683
JayJayDee <jindongp@gmail.com>
2019-02-19 16:26:16 +0900

    added test-asset. added splash scene.

--- lib/scenes/splash_scene.dart
+++ lib/scenes/splash_scene.dart
@@ -6,9 +6,40 @@ class SplashScene extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: Styles.appBackground,
-      child: Center(
-        child: Text('Test App'),
-      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Text('Chatpot', textScaleFactor: 1.5)
+                  ),
+                  Image(
+                    image:AssetImage('assets/test_logo.png'),
+                    width: 150,
+                    height: 150,
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: CupertinoActivityIndicator(),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
