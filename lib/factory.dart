ffe7069c13dd5e595108e4287a11ab38fddf006d
JayJayDee <jindongp@gmail.com>
2019-02-19 14:58:02 +0900

    added implementation for pref_auth_accessor. added factory instances.

--- lib/factory.dart
+++ lib/factory.dart
@@ -1,11 +1,27 @@
 import 'package:chatpot_app/storage/auth_accessor.dart';
+import 'package:chatpot_app/apis/requester.dart';
 import 'package:chatpot_app/storage/pref_auth_accessor.dart';
+import 'package:chatpot_app/apis/default_requester.dart';
 
 Map<String, dynamic> _instances;
 
 void initFactory() {
   _instances = new Map<String, dynamic>();
   _instances['AuthAccessor'] = PrefAuthAccessor();
+  _instances['MemberRequester'] = _initMemberRequester();
+  _instances['RoomRequester'] = _initRoomRequseter();
 }
 
+Requester _initMemberRequester() => DefaultRequester(
+  accessor: authAccessor(),
+  baseUrl: 'http://dev-auth.chatpot.chat'
+);
+
+Requester _initRoomRequseter() => DefaultRequester(
+  accessor: authAccessor(),
+  baseUrl: 'http://dev-room.chatpot.chat'
+);
+
 AuthAccessor authAccessor() => _instances['AuthAccessor'];
+Requester memberRequester() => _instances['MemberRequester'];
+Requester roomRequester() => _instances['RoomRequester'];
--- lib/storage/pref_auth_accessor.dart
+++ lib/storage/pref_auth_accessor.dart
@@ -11,26 +11,32 @@ Future<SharedPreferences> _getPrefs() async {
 
 class PrefAuthAccessor implements AuthAccessor {
   Future<String> getToken() async {
-    return '';
+    var prefs = await _getPrefs();
+    return prefs.getString('TOKEN');
   }
 
   Future<void> setToken(String token) async {
-
+    var prefs = await _getPrefs();
+    await prefs.setString('TOKEN', token);
   }
 
   Future<String> getSessionKey() async {
-    return '';
+    var prefs = await _getPrefs();
+    return prefs.getString('SESSION');
   }
 
   Future<void> setSessionKey(String sessionKey) async {
-
+    var prefs = await _getPrefs();
+    await prefs.setString('SESSION', sessionKey);
   }
 
   Future<String> getPassword() async {
-    return '';
+    var prefs = await _getPrefs();
+    return prefs.getString('PASSWORD');
   }
 
   Future<void> setPassword(String password) async {
-
+    var prefs = await _getPrefs();
+    await prefs.setString('PASSWORD', password);
   }
 }
