3d4afcace4cc536e962b7472ea8577d6274e97cc
JayJayDee <jindongp@gmail.com>
2019-02-20 14:48:16 +0900

    set misc_accessor definitions and its implementations.

---
+++ lib/storage/pref_misc_accessor.dart
@@ -0,0 +1,10 @@
+import 'package:chatpot_app/storage/misc_accessor.dart';
+
+class PrefMiscAccessor extends MiscAccessor {
+  Future<bool> isFirstTime() async {
+    return false;
+  }
+  Future<void> setNotFirstTime() async {
+    
+  }
+}
