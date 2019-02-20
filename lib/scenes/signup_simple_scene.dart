7c64e5b7f6f54b0ec48895601bc6fc48fc53fee0
JayJayDee <jindongp@gmail.com>
2019-02-20 11:24:37 +0900

    added simple-sign-up scene implementations.

--- lib/scenes/signup_simple_scene.dart
+++ lib/scenes/signup_simple_scene.dart
@@ -11,7 +11,32 @@ class SimpleSignupScene extends StatelessWidget {
       ),
       child: SafeArea(
         child: Column(
-          
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text("Just start without complicated signing up."),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text("But remember, signing up with E-mail allows you to use the Chatpot on other devices."),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text('You can also register your mail account later.'),
+                  Padding(padding: EdgeInsets.only(top: 20)),
+                  CupertinoButton(
+                    color: CupertinoColors.activeBlue,
+                    child: Text('Start without signup!'),
+                    onPressed: () {
+
+                    },
+                  )
+                ]
+              )
+            )
+          ],
         ),
       )
     );
