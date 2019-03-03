ddcfe6e7200ea8550933af62e240982c93f585b7
JayJayDee <jindongp@gmail.com>
2019-03-03 15:22:46 +0900

    fixed locale problem in iOS. added chatting row subtitle translation.

--- lib/utils/locale_converter.dart
+++ lib/utils/locale_converter.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:chatpot_app/entities/member.dart';
+import 'package:chatpot_app/entities/room.dart';
 
 class LocaleConverter {
   String _language;
@@ -19,4 +20,40 @@ class LocaleConverter {
     enNick.split(' ')
       .map((String token) => '${token[0].toUpperCase()}${token.substring(1)}')
       .join(' ');
+
+  String roomSubtitle(Room room) {
+    String ownerNick = getNick(room.owner.nick);
+    int remain = room.numAttendee - 1;
+    if (_language == 'ko') {
+      if (remain == 0) return "$ownerNick님 혼자 있습니다.";
+      return "$ownerNick님 외 $remain명";
+
+    } else if (_language == 'en') {
+      if (remain == 0) return "$ownerNick alone";
+      return "$ownerNick and $remain others";
+
+    } else if (_language == 'ja') {
+      if (remain == 0) return "$ownerNickさんだけです。";
+      return "$ownerNickさん他$remain人がいます。";
+    }
+    return '';
+  }
+
+  String myRoomSubtitle(MyRoom room) {
+    String ownerNick = getNick(room.owner.nick);
+    int remain = room.numAttendee - 1;
+    if (_language == 'ko') {
+      if (remain == 0) return "$ownerNick님 혼자 있습니다.";
+      return "$ownerNick님 외 $remain명";
+
+    } else if (_language == 'en') {
+      if (remain == 0) return "$ownerNick alone";
+      return "$ownerNick and $remain others";
+
+    } else if (_language == 'ja') {
+      if (remain == 0) return "$ownerNickさんだけです。";
+      return "$ownerNickさん他$remain人がいます。";
+    }
+    return '';
+  }
 }
