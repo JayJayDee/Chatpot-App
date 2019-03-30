448fa57ed3cc3a41104565352abb20ae8702b1dd
JayJayDee <jindongp@gmail.com>
2019-03-30 18:23:05 +0900

    added AssetUploadResp.

--- lib/apis/asset_api.dart
+++ lib/apis/asset_api.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:meta/meta.dart';
 import 'package:chatpot_app/apis/requester.dart';
+import 'package:chatpot_app/apis/api_entities.dart';
+
+typedef UploadProgressCallback (int);
 
 class AssetApi {
   Requester _requester;
@@ -8,8 +12,13 @@ class AssetApi {
   AssetApi({
     @required Requester requester
   }) {
-    _requester =requester;
+    _requester = requester;
   }
 
-  // TODO: image upload to be implemented.
+  Future<AssetUploadResp> requestImageUpload(File file, {
+    UploadProgressCallback uploadCallback
+  }) async {
+    // TODO: upload required.
+    return null;
+  }
 }
