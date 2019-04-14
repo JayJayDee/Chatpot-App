4540718f6e15773870dd2640dd986b608ba1619a
JayJayDee <jindongp@gmail.com>
2019-04-14 10:34:06 +0900

    added photo-detail-scene.

---
+++ lib/scenes/photo_detail_scene.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class PhotoDetailScene extends StatefulWidget {  
+  @override
+  _PhotoDetailSceneState createState() => _PhotoDetailSceneState();
+}
+
+class _PhotoDetailSceneState extends State<PhotoDetailScene> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
