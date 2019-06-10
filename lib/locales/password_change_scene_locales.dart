2bc199941ded87358960686986e0d242a5014f03
JayJayDee <jindongp@gmail.com>
2019-06-10 12:28:13 +0900

    added implementation for password_change_scene.

--- lib/locales/password_change_scene_locales.dart
+++ lib/locales/password_change_scene_locales.dart
@@ -14,4 +14,13 @@ class PasswordChangeSceneLocales {
     else if (language == 'ja') return 'パスワードを変更する';
     return 'Change password';
   }
+
+  String get description {
+    if (language == 'ko') return '''로그인에 사용하는 비밀번호를 변경합니다.
+기존 비밀번호와 지금부터 사용할 새로운 패스워드를 입력해 주세요.''';
+    else if (language == 'ja') return '''サインインに使用するパスワードを変更する
+既存のパスワードと今後使用する新しいパスワードを入力してください。''';
+    return '''Change the password you use to sign in.
+Please enter your existing password and a new password to use from now on.''';
+  }
 }
--- lib/scenes/password_change_scene.dart
+++ lib/scenes/password_change_scene.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:chatpot_app/factory.dart';
+import 'package:chatpot_app/styles.dart';
 
 class PasswordChangeScene extends StatefulWidget {
 
@@ -20,9 +21,40 @@ class _PasswordChangeSceneState extends State<PasswordChangeScene> {
       child: SafeArea(
         child: Stack(
           alignment: Alignment.center,
-          children: []
+          children: [
+            ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 10, top: 10, right: 10),
+                  child: Text(locales().passwordChange.description,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Styles.primaryFontColor
+                    )
+                  )
+                )
+              ]
+            ),
+            _buildProgress(context)
+          ]
         )
       )
     );
   }
+}
+
+Widget _buildOldPasswordField(BuildContext context) {
+  return Container();
+}
+
+Widget _buildNewPasswordField(BuildContext context) {
+  return Container();
+}
+
+Widget _buildNewPasswordConfirmField(BuildContext context) {
+  return Container();
+}
+
+Widget _buildProgress(BuildContext context) {
+  return Container();
 }
