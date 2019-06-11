9fba1b2b13549b3bd3e5b365ba6abb1af44cd121
JayJayDee <jindongp@gmail.com>
2019-06-11 20:32:03 +0900

    added member_detail_sheet for message_scene.

---
+++ lib/components/member_detail_sheet.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
+import 'package:flutter/widgets.dart';
+
+Future<void> showMemberDetailSheet(BuildContext context, {
+  @required String memberToken 
+}) async {
+
+}
+
+class _MemberDetailSheet extends StatefulWidget {
+
+  @override
+  State createState() => _MemberDetailSheetState();
+}
+
+class _MemberDetailSheetState extends State<_MemberDetailSheet> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
