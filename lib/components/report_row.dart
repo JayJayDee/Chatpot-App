4cb7f69bff331aa5a2a021b70d55ec726a62dc20
JayJayDee <jindongp@gmail.com>
2019-06-23 14:33:29 +0900

    completed report_history_scene.

--- lib/components/report_row.dart
+++ lib/components/report_row.dart
@@ -3,6 +3,7 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:meta/meta.dart';
 import 'package:chatpot_app/entities/report.dart';
 import 'package:chatpot_app/styles.dart';
+import 'package:chatpot_app/factory.dart';
 
 class ReportRow extends StatelessWidget {
 
@@ -15,12 +16,62 @@ class ReportRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: CupertinoColors.inactiveGray,
+            width: 0.3
+          )
+        )
+      ),
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(_buildStatusIcon(report.status),
-            color: Styles.secondaryFontColor,
-            size: 30
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(locales().reportHistoryScene.reportTitle(report),
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Styles.primaryFontColor
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 7)),
+                  Text(locales().reportHistoryScene.reportDescription(report.status),
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Styles.secondaryFontColor
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 7)),
+                  Text(locales().reportHistoryScene.reportTimeUtc(report.regDate),
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Styles.secondaryFontColor
+                    )
+                  )
+                ]
+              )
+            )
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(_buildStatusIcon(report.status),
+                color: Styles.secondaryFontColor,
+                size: 24
+              ),
+              Padding(padding: EdgeInsets.only(left: 5)),
+              Text(locales().reportHistoryScene.reportExpr(report.status),
+                style: TextStyle(
+                  color: Styles.secondaryFontColor,
+                  fontSize: 16
+                ),
+              )
+            ]
           )
         ]
       )
@@ -29,7 +80,7 @@ class ReportRow extends StatelessWidget {
 }
 
 IconData _buildStatusIcon(ReportState state) =>
-  state == ReportState.REPORTED ? MdiIcons.account : // TODO: to be changed
+  state == ReportState.REPORTED ? MdiIcons.alert :
   state == ReportState.IN_PROGRESS ? MdiIcons.refresh :
   state == ReportState.DONE ? MdiIcons.check :
   null;
--- lib/locales/report_history_scene_locales.dart
+++ lib/locales/report_history_scene_locales.dart
@@ -1,3 +1,4 @@
+import 'package:chatpot_app/entities/report.dart';
 import 'package:chatpot_app/locales/root_locale_converter.dart';
 
 class ReportHistorySceneLocales {
@@ -13,5 +14,54 @@ class ReportHistorySceneLocales {
     if (language == 'ko') return '내 신고 내역';
     else if (language == 'ja') return '私の報告履歴';
     return 'My reporting history';
+  }
+
+  String reportExpr(ReportState state) {
+    if (state == ReportState.REPORTED) {
+      if (language == 'ko') return '신고완료';
+      else if (language == 'ja') return '申告完了';
+      return 'Reported';
+    } else if (state == ReportState.IN_PROGRESS) {
+      if (language == 'ko') return '진행중';
+      else if (language == 'ja') return '進行中';
+      return 'Progress';
+    } else if (state == ReportState.DONE) {
+      if (language == 'ko') return '처리완료';
+      else if (language == 'ja') return '申告完了';
+      return 'Done';
+    }
+    return '';
+  }
+
+  String reportDescription(ReportState state) {
+    if (state == ReportState.REPORTED) {
+      if (language == 'ko') {
+        return '''신고가 완료되었으며, Chatpot 운영자의 조회를 대기하고 있습니다. 최대 24시간 내에 조회 및 조치 결과를 알려드립니다.''';
+      } else if (language == 'ja') {
+        return '申告完了';
+      }
+      return '''Your report has been completed and waiting for Chatpot administrator to view it. We will notify you of inquiries and action results within 24 hours.''';
+    }
+    return '';
+  }
+
+  String reportTimeUtc(String time) {
+    if (language == 'ko') return """신고시간 (UTC):
+$time""";
+    else if (language == 'ja') return """申告時刻（UTC）：
+$time""";
+    return """Report time (UTC):
+$time""";
+  }
+
+  String reportTitle(ReportStatus report) {
+    String reportType = '';
+    if (report.reportType == ReportType.HATE) reportType = root.reportScene.hateLabel;
+    else if (report.reportType == ReportType.SEXUAL) reportType = root.reportScene.sexualLabel;
+    else reportType = root.reportScene.etcLabel;
+
+    if (language == 'ko') return "$reportType에 대한 신고";
+    else if (language == 'ja') return "$reportTypeに対する申告";
+    return "Report about $reportType";
   }
 }
