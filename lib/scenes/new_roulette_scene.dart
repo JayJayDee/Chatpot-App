7dcddf303319fb7684939b3000e8f661e9a56938
JayJayDee <jindongp@gmail.com>
2019-07-29 22:39:38 +0900

    fixed text in message_scene.

---
+++ lib/scenes/new_roulette_scene.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class NewRouletteScene extends StatefulWidget {
+  @override
+  State createState() => _NewRouletteSceneState();
+}
+
+class _NewRouletteSceneState extends State<NewRouletteScene> {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
