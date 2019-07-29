08f2c86c98c2ad08ec282c7660f6994bd9a74530
JayJayDee <jindongp@gmail.com>
2019-07-29 21:44:52 +0900

    added roulette-chat processes on chats_scene.

--- lib/locales/chats_scene_locales.dart
+++ lib/locales/chats_scene_locales.dart
@@ -1,4 +1,5 @@
 import 'package:chatpot_app/locales/root_locale_converter.dart';
+import 'package:chatpot_app/entities/member.dart';
 
 class ChatsSceneLocales {
   String language;
@@ -28,5 +29,11 @@ class ChatsSceneLocales {
     return '''You have no chat rooms.
 Select a room in the Lounge,
 Or you can open a new chat.''';
+  }
+
+  String rouletteTitle(Nick nick) {
+    if (language == 'ko') return "${root.getNick(nick)}님과의 랜덤채팅";
+    else if (language == 'ja') return "${root.getNick(nick)}さんのランダムチャット";
+    return "Roulette chat with ${root.getNick(nick)}";
   }
 }
